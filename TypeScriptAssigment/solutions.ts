5b4106d97efcf3fac12af2b7f43a0dd635403a91
naimsheikh8020 <naimsheikh8020@gmail.com>
2026-05-07 12:22:39 +0600

    Problem 4 done

--- TypeScriptAssigment/solutions.ts
+++ TypeScriptAssigment/solutions.ts
@@ -28,3 +28,10 @@ function checkType(value: StringOrNumber): string {
 checkType(42)
 checkType("Hello")
 
+//problem 4 
+
+function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+    return obj[key];
+}
+const user = { id: 1, name: "John Doe", age: 21 };
+console.log(getProperty(user, "name"));
