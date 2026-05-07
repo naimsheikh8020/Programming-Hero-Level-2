bda851da43bbd59313d0eb7ea8c0ad62314d49ff
naimsheikh8020 <naimsheikh8020@gmail.com>
2026-05-07 12:08:04 +0600

    problem 1,2,3  done

---
+++ TypeScriptAssigment/solutions.ts
@@ -0,0 +1,30 @@
+//Problem 1:
+function filterEvenNumbers(numbers: number[]): number[] {
+    return numbers.filter((num) => num % 2 === 0);
+}
+
+filterEvenNumbers([1, 2, 3, 4, 5, 6])
+
+//Problem 2:
+function reverseString(str: string): string {
+    let reversed : string = "";
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+
+    return reversed
+}
+
+reverseString("typescript")
+
+//Problem 3 
+type StringOrNumber = string | number;
+
+function checkType(value: StringOrNumber): string {
+    return typeof value === "number" ? "Number": "String";
+}
+
+checkType(42)
+checkType("Hello")
+
