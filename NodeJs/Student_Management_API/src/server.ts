4a27e691714d69734076ec1c3350943cbdfe8e4b
naimsheikh8020 <naimsheikh8020@gmail.com>
2026-05-15 23:49:40 +0600

    get data and post data done

---
+++ NodeJs/Student_Management_API/src/server.ts
@@ -0,0 +1,94 @@
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+import { Pool } from "pg";
+import config from "./config/index.js";
+
+const app: Application = express();
+
+app.use(express.json());
+const PORT = config.port;
+
+const pool = new Pool({
+  connectionString: config.connection_string,
+});
+
+const initDB = async () => {
+  try {
+    await pool.query(`
+      CREATE TABLE  IF NOT EXISTS Students(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(20) NOT NULL,
+      email VARCHAR(20) UNIQUE NOT NULL,
+      course VARCHAR(20) NOT NULL,
+       age INT NOT NULL CHECK(age > 0),
+      
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+      )
+    `);
+    console.log("Data base Connect successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+initDB();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World from Express!");
+});
+
+app.post("/api/students", async (req: Request, res: Response) => {
+  try {
+    const { name, email, course, age } = req.body;
+    const result = await pool.query(
+      `
+      INSERT INTO students (name, email, course, age)
+      VALUES($1, $2, $3, $4)
+      RETURNING *
+      `,
+      [name, email, course, age],
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "User Created Successfully",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+
+
+app.get('/api/students', async(req:Request, res:Response)=>{
+  try {
+    const result = await pool.query(`
+      SELECT * FROM students;
+      `)
+     res.status(200).json({
+      success: true,
+      message: "All the students retrive successfuly",
+      data: result.rows
+     })
+  } catch (error : any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+})
+
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
