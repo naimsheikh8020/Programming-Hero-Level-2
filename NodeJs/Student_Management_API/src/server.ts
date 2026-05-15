14e1f059eca5a64ff635e0b733a797c75c1df1cf
naimsheikh8020 <naimsheikh8020@gmail.com>
2026-05-16 00:21:44 +0600

    Patch Method Done

--- NodeJs/Student_Management_API/src/server.ts
+++ NodeJs/Student_Management_API/src/server.ts
@@ -66,27 +66,83 @@ app.post("/api/students", async (req: Request, res: Response) => {
   }
 });
 
-
-
-app.get('/api/students', async(req:Request, res:Response)=>{
+app.get("/api/students", async (req: Request, res: Response) => {
   try {
     const result = await pool.query(`
       SELECT * FROM students;
-      `)
-     res.status(200).json({
+      `);
+    res.status(200).json({
       success: true,
       message: "All the students retrive successfuly",
-      data: result.rows
-     })
-  } catch (error : any) {
+      data: result.rows,
+    });
+  } catch (error: any) {
     res.status(500).json({
       success: false,
       message: error.message,
     });
   }
-})
+});
 
+app.get("/api/students/:id", async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const result = await pool.query(
+      `
+      SELECT * FROM users WHERE id=$1
+      `,
+      [id],
+    );
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        message: "User Not Found",
+        data: {},
+      });
+    }
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
 
+app.patch("/api/students/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { name, email, course, age } = req.body;
+  try {
+    const checkUser = await pool.query(`SELECT * FROM users WHERE id = $1`, [
+      id,
+    ]);
+    if (checkUser.rows.length === 0) {
+      return res.status(404).json({
+        message: "User Not Found",
+        data: {},
+      });
+    }
+    const result = await pool.query(`
+      UPDATE students
+      SET
+        name = COALESCE($1, name),
+        email = COALESCE($2, email),
+        course = COALESCE($3, course),
+        age = COALESCE($4, age),
+        updated_at = NOW()
+      WHERE id = $5
+      RETURNING *`,[name, email, course,  age, id]);
+      res.status(200).json({
+      success: true,
+      message: "User updated successfully",
+      data: result.rows[0],
+    });
+
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
 
 // Start the server
 app.listen(PORT, () => {
