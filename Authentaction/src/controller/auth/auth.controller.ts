85c3c8a3dd4920d1c9bdbc14ae054dd95644fc24
naimsheikh8020 <naimsheikh8020@gmail.com>
2026-06-25 23:36:39 +0600

    User register handler added

--- Authentaction/src/controller/auth/auth.controller.ts
+++ Authentaction/src/controller/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from "express";
 import { registerSchema } from "./auth.schema.js";
 import { User } from "../../models/user.model.js";
+import { hashPassword } from "../../lib/hash.js";
 
 
 
@@ -24,6 +25,23 @@ export const registerHandler = async (req: Request, res: Response) => {
         message: "User already exists"
       });
     }
+
+    const passwordHash = await hashPassword(password);
+
+    const newlyCreatedUser = await User.create({
+      email: NormalizedEmail,
+      passwordHash: passwordHash,
+      name,
+      role: "user",
+      isEmailVerified: false,
+      twoFactorEnabled: false
+    });
+
+    return res.status(201).json({
+      status: "success",
+      message: "User created successfully",
+      data: newlyCreatedUser
+    });
 
   } catch (error) {
     return res.status(500).json({
